b20c4d2c56e7439297d2c84f6a1d573890b53a4c
Tales Figueiredo (CTW) <tales.figueiredo@ctw.bmwgroup.com>
2025-12-15 16:50:42 +0000

    From zero with errors

--- src/test/java/com/ctw/service/VehicleServiceTest.java
+++ src/test/java/com/ctw/service/VehicleServiceTest.java
@@ -75,7 +75,7 @@ class VehicleServiceTest {
     @Test
     void testGetById() {
         Engine engine = new Engine();
-        engine.setId("engine-1");
+        engine.setId("engine-2");
         Vehicle vehicle = new Vehicle();
         vehicle.setId("v1");
         vehicle.setName("Car");
